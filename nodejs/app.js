ee3a46457319d08d5d4eedcfe06587e80f8fec2a
dbrosy <dbrosy@hotmail.com>
2017-05-12 20:34:13 +1000

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,13 +1,17 @@
 var static = require('node-static');
-
-var publicfiles = new (static.Server)('./public');
-
+    
+var fileServer = new static.Server('./public');
+ 
 require('http').createServer(function (request, response) {
-
-	request.addListener('end', function () {
-		publicfiles.serve(request, response);
-	});
-
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file 
+                console.error("Error serving " + request.url + " - " + err.message);
+ 
+                // Respond to the client 
+                response.writeHead(err.status, err.headers);
+                response.end();
+            }
+        });
+    }).resume();
 }).listen(8090);
-
-console.log('Navigate to http://localhost:8090/.');
